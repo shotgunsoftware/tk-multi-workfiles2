8373b15571b2567382cfd41b5b1e35d2c3722ca7
Alan Dann <alan.dann@shotgunsoftware.com>
2013-04-18 13:15:56 +0100

    Removed launch_on_startup option until we've fixed the issue with the nuke engine.  Removed allow_task_creation option until it's implemented

--- app.py
+++ app.py
@@ -30,15 +30,6 @@ class MultiWorkFiles(tank.platform.Application):
         cmd = lambda app=self: tk_multi_workfiles.Versioning.show_change_version_dlg(app)
         self.engine.register_command("Version up Current Scene...", cmd)
         
-        # only launch the dialog once at startup
-        # use tank object to store this flag
-        if not hasattr(tank, '_tk_multi_workfiles_shown'):
-            # very first time we run this app
-            tank._tk_multi_workfiles_shown = True
-            # show the UI at startup - but only if the engine supports a UI
-            if self.get_setting('launch_at_startup') and self.engine.has_ui:
-                self._work_files_handler.show_dlg()
-        
     def destroy_app(self):
         self.log_debug("Destroying tk-multi-workfiles")
         self._work_files_handler = None
