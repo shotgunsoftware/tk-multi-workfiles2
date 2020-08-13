51a23be3452ff1d93e6b41bc42eda16b485ec0d6
rob-aitchison <rob.aitchison@autodesk.com>
2020-08-13 10:20:25 -0400

    Added default scene_operation_tk-sketchbook.py hook Also updated .gitignore for Pycharm files

---
+++ hooks/scene_operation_tk-sketchbook.py
@@ -0,0 +1,100 @@
+# Copyright (c) 2020 Autodesk, Inc.
+#
+# CONFIDENTIAL AND PROPRIETARY
+#
+# This work is provided "AS IS" and subject to the Shotgun Pipeline Toolkit
+# Source Code License included in this distribution package. See LICENSE.
+# By accessing, using, copying or modifying this work you indicate your
+# agreement to the Shotgun Pipeline Toolkit Source Code License. All rights
+# not expressly granted therein are reserved by Autodesk, Inc.
+#
+
+import sgtk
+
+import sketchbook_api
+
+HookClass = sgtk.get_hook_baseclass()
+
+
+class SceneOperation(HookClass):
+    """
+    Hook called to perform an operation with the
+    current scene
+    """
+
+    def execute(
+        self,
+        operation,
+        file_path,
+        context,
+        parent_action,
+        file_version,
+        read_only,
+        **kwargs
+    ):
+        """
+        Main hook entry point
+
+        :param operation:       String
+                                Scene operation to perform
+
+        :param file_path:       String
+                                File path to use if the operation
+                                requires it (e.g. open)
+
+        :param context:         Context
+                                The context the file operation is being
+                                performed in.
+
+        :param parent_action:   This is the action that this scene operation is
+                                being executed for.  This can be one of:
+                                - open_file
+                                - new_file
+                                - save_file_as
+                                - version_up
+
+        :param file_version:    The version/revision of the file to be opened.  If this is 'None'
+                                then the latest version should be opened.
+
+        :param read_only:       Specifies if the file should be opened read-only or not
+
+        :returns:               Depends on operation:
+                                'current_path' - Return the current scene
+                                                 file path as a String
+                                'reset'        - True if scene was reset to an empty
+                                                 state, otherwise False
+                                all others     - None
+        """
+        if operation == "current_path":
+            """
+            Get current file path
+            """
+            return sketchbook_api.get_current_path()
+
+        elif operation == "open":
+            """
+            File Open
+            """
+            sketchbook_api.open_file(file_path)
+            return True
+
+        elif operation == "save":
+            """
+            File Save
+            """
+            sketchbook_api.save_file()
+            return True
+
+        elif operation == "save_as":
+            """
+            File Save As
+            """
+            sketchbook_api.save_file_as(file_path)
+            return True
+
+        elif operation == "reset":
+            """
+            Reset the scene to an empty state
+            """
+            sketchbook_api.reset()
+            return True
