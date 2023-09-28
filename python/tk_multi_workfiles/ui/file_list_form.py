6a490421b3a23340a5ba34ad4ad3c83f8554c3a6
Stacey Oue <stacey.oue@autodesk.com>
2023-09-28 10:33:58 -0700

    SG-31598: List view (#132)
    * Add hook to configure the file view UI
    * Add a list view to the main file view
    * Update My Tasks list view to be consistent with the the file view
    * Add view mode buttons to toggle between the view modes
    * Add settings manager to save/restore the view state
    * Remove obsolete classes and ui
    * Update style.qss

--- python/tk_multi_workfiles/ui/file_list_form.py
+++ python/tk_multi_workfiles/ui/file_list_form.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'file_list_form.ui'
+# Form implementation generated from reading ui file 'C:\Users\qa\sg_envs\dv\tk\tk-multi-workfiles2\resources\file_list_form.ui'
 #
+# Created: Tue Aug 15 10:03:02 2023
 #      by: pyside-uic 0.2.15 running on PySide 1.2.2
 #
 # WARNING! All changes made in this file will be lost!
@@ -16,51 +17,55 @@ class Ui_FileListForm(object):
         self.verticalLayout.setSpacing(4)
         self.verticalLayout.setContentsMargins(2, 6, 2, 2)
         self.verticalLayout.setObjectName("verticalLayout")
+        self.horizontalLayout_8 = QtGui.QHBoxLayout()
+        self.horizontalLayout_8.setObjectName("horizontalLayout_8")
+        self.verticalLayout_2 = QtGui.QVBoxLayout()
+        self.verticalLayout_2.setObjectName("verticalLayout_2")
         self.horizontalLayout_3 = QtGui.QHBoxLayout()
         self.horizontalLayout_3.setContentsMargins(1, -1, 1, -1)
         self.horizontalLayout_3.setObjectName("horizontalLayout_3")
         self.user_filter_btn = UserFilterButton(FileListForm)
         self.user_filter_btn.setStyleSheet("#user_filter_btn {\n"
-"    width: 40;\n"
-"    height: 24;\n"
-"    border: 0px, none;\n"
-"    border-image: url(:/tk-multi-workfiles2/users_none.png);\n"
+"       width: 40;\n"
+"       height: 24;\n"
+"       border: 0px, none;\n"
+"       border-image: url(:/tk-multi-workfiles2/users_none.png);\n"
 "}\n"
 "#user_filter_btn::hover, #user_filter_btn::pressed {\n"
-"    border-image: url(:/tk-multi-workfiles2/users_none_hover.png);\n"
+"       border-image: url(:/tk-multi-workfiles2/users_none_hover.png);\n"
 "}\n"
 "\n"
 "#user_filter_btn[user_style=\"none\"] {\n"
-"    border-image: url(:/tk-multi-workfiles2/users_none.png);\n"
+"       border-image: url(:/tk-multi-workfiles2/users_none.png);\n"
 "}\n"
 "#user_filter_btn[user_style=\"current\"] {\n"
-"    border-image: url(:/tk-multi-workfiles2/users_current.png);\n"
+"       border-image: url(:/tk-multi-workfiles2/users_current.png);\n"
 "}\n"
 "#user_filter_btn[user_style=\"other\"] {\n"
-"    border-image: url(:/tk-multi-workfiles2/users_other.png);\n"
+"       border-image: url(:/tk-multi-workfiles2/users_other.png);\n"
 "}\n"
 "#user_filter_btn[user_style=\"all\"] {\n"
-"    border-image: url(:/tk-multi-workfiles2/users_all.png);\n"
+"       border-image: url(:/tk-multi-workfiles2/users_all.png);\n"
 "}\n"
 "\n"
 "#user_filter_btn::hover[user_style=\"none\"], #user_filter_btn::pressed[user_style=\"none\"] {\n"
-"    border-image: url(:/tk-multi-workfiles2/users_none_hover.png);\n"
+"       border-image: url(:/tk-multi-workfiles2/users_none_hover.png);\n"
 "}\n"
 "#user_filter_btn::hover[user_style=\"current\"], #user_filter_btn::pressed[user_style=\"current\"] {\n"
-"    border-image: url(:/tk-multi-workfiles2/users_current_hover.png);\n"
+"       border-image: url(:/tk-multi-workfiles2/users_current_hover.png);\n"
 "}\n"
 "#user_filter_btn::hover[user_style=\"other\"], #user_filter_btn::pressed[user_style=\"other\"] {\n"
-"    border-image: url(:/tk-multi-workfiles2/users_other_hover.png);\n"
+"       border-image: url(:/tk-multi-workfiles2/users_other_hover.png);\n"
 "}\n"
 "#user_filter_btn::hover[user_style=\"all\"], #user_filter_btn::pressed[user_style=\"all\"] {\n"
-"    border-image: url(:/tk-multi-workfiles2/users_all_hover.png);\n"
+"       border-image: url(:/tk-multi-workfiles2/users_all_hover.png);\n"
 "}\n"
 "\n"
 "#user_filter_btn::menu-indicator, #user_filter_btn::menu-indicator::pressed, #user_filter_btn::menu-indicator::open {\n"
-"    left: -2px;\n"
-"    top: -2px;\n"
-"    width: 8px;\n"
-"    height: 6px;\n"
+"       left: -2px;\n"
+"       top: -2px;\n"
+"       width: 8px;\n"
+"       height: 6px;\n"
 "}\n"
 "")
         self.user_filter_btn.setText("")
@@ -75,6 +80,20 @@ class Ui_FileListForm(object):
         self.horizontalLayout_3.addWidget(self.check_refs_cb)
         spacerItem = QtGui.QSpacerItem(40, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
         self.horizontalLayout_3.addItem(spacerItem)
+        self.thumbnail_mode = QtGui.QToolButton(FileListForm)
+        self.thumbnail_mode.setMinimumSize(QtCore.QSize(26, 26))
+        self.thumbnail_mode.setText("")
+        self.thumbnail_mode.setCheckable(True)
+        self.thumbnail_mode.setChecked(False)
+        self.thumbnail_mode.setObjectName("thumbnail_mode")
+        self.horizontalLayout_3.addWidget(self.thumbnail_mode)
+        self.list_mode = QtGui.QToolButton(FileListForm)
+        self.list_mode.setMinimumSize(QtCore.QSize(26, 26))
+        self.list_mode.setText("")
+        self.list_mode.setCheckable(True)
+        self.list_mode.setChecked(True)
+        self.list_mode.setObjectName("list_mode")
+        self.horizontalLayout_3.addWidget(self.list_mode)
         self.search_ctrl = SearchWidget(FileListForm)
         self.search_ctrl.setMinimumSize(QtCore.QSize(100, 0))
         self.search_ctrl.setStyleSheet("#search_ctrl {\n"
@@ -82,8 +101,8 @@ class Ui_FileListForm(object):
 "}")
         self.search_ctrl.setObjectName("search_ctrl")
         self.horizontalLayout_3.addWidget(self.search_ctrl)
-        self.horizontalLayout_3.setStretch(3, 1)
-        self.verticalLayout.addLayout(self.horizontalLayout_3)
+        self.horizontalLayout_3.setStretch(2, 1)
+        self.verticalLayout_2.addLayout(self.horizontalLayout_3)
         self.view_pages = QtGui.QStackedWidget(FileListForm)
         self.view_pages.setObjectName("view_pages")
         self.list_page = QtGui.QWidget()
@@ -91,7 +110,7 @@ class Ui_FileListForm(object):
         self.horizontalLayout_5 = QtGui.QHBoxLayout(self.list_page)
         self.horizontalLayout_5.setContentsMargins(0, 0, 0, 0)
         self.horizontalLayout_5.setObjectName("horizontalLayout_5")
-        self.file_list_view = GroupedListView(self.list_page)
+        self.file_list_view = GroupedItemView(self.list_page)
         self.file_list_view.setStyleSheet("")
         self.file_list_view.setObjectName("file_list_view")
         self.horizontalLayout_5.addWidget(self.file_list_view)
@@ -105,8 +124,28 @@ class Ui_FileListForm(object):
         self.file_details_view.setObjectName("file_details_view")
         self.horizontalLayout_4.addWidget(self.file_details_view)
         self.view_pages.addWidget(self.details_page)
-        self.verticalLayout.addWidget(self.view_pages)
-        self.verticalLayout.setStretch(1, 1)
+        self.verticalLayout_2.addWidget(self.view_pages)
+        self.horizontalLayout = QtGui.QHBoxLayout()
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        spacerItem1 = QtGui.QSpacerItem(40, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
+        self.horizontalLayout.addItem(spacerItem1)
+        self.item_size_slider = QtGui.QSlider(FileListForm)
+        self.item_size_slider.setMinimumSize(QtCore.QSize(150, 0))
+        self.item_size_slider.setMaximumSize(QtCore.QSize(150, 16777215))
+        self.item_size_slider.setStyleSheet(" QSlider::handle:horizontal {\n"
+"       border: 1px solid palette(base);\n"
+"     border-radius: 3px;\n"
+"     width: 4px;\n"
+"     background: palette(light);\n"
+" }")
+        self.item_size_slider.setMinimum(20)
+        self.item_size_slider.setMaximum(200)
+        self.item_size_slider.setOrientation(QtCore.Qt.Horizontal)
+        self.item_size_slider.setObjectName("item_size_slider")
+        self.horizontalLayout.addWidget(self.item_size_slider)
+        self.verticalLayout_2.addLayout(self.horizontalLayout)
+        self.horizontalLayout_8.addLayout(self.verticalLayout_2)
+        self.verticalLayout.addLayout(self.horizontalLayout_8)
 
         self.retranslateUi(FileListForm)
         self.view_pages.setCurrentIndex(0)
@@ -117,9 +156,13 @@ class Ui_FileListForm(object):
         self.user_filter_btn.setProperty("user_style", QtGui.QApplication.translate("FileListForm", "current", None, QtGui.QApplication.UnicodeUTF8))
         self.all_versions_cb.setText(QtGui.QApplication.translate("FileListForm", "All Versions", None, QtGui.QApplication.UnicodeUTF8))
         self.check_refs_cb.setText(QtGui.QApplication.translate("FileListForm", "Check References on Open", None, QtGui.QApplication.UnicodeUTF8))
+        self.thumbnail_mode.setToolTip(QtGui.QApplication.translate("FileListForm", "Thumbnail Mode", None, QtGui.QApplication.UnicodeUTF8))
+        self.thumbnail_mode.setAccessibleName(QtGui.QApplication.translate("FileListForm", "thumbnail_mode", None, QtGui.QApplication.UnicodeUTF8))
+        self.list_mode.setToolTip(QtGui.QApplication.translate("FileListForm", "List Mode", None, QtGui.QApplication.UnicodeUTF8))
+        self.list_mode.setAccessibleName(QtGui.QApplication.translate("FileListForm", "list_mode", None, QtGui.QApplication.UnicodeUTF8))
         self.search_ctrl.setAccessibleName(QtGui.QApplication.translate("FileListForm", "Search All Files", None, QtGui.QApplication.UnicodeUTF8))
 
 from ..file_list.file_details_view import FileDetailsView
 from ..file_list.user_filter_button import UserFilterButton
-from ..framework_qtwidgets import GroupedListView, SearchWidget
+from ..framework_qtwidgets import SearchWidget, GroupedItemView
 from . import resources_rc
