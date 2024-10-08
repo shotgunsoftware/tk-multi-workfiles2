60a1dae6d316552205f58b6714e94650f024c187
Carlos Villavicencio <carlos.villavicencio@autodesk.com>
2024-10-08 13:05:07 -0500

    Fix ui sources

--- python/tk_multi_workfiles/ui/entity_tree_form.py
+++ python/tk_multi_workfiles/ui/entity_tree_form.py
@@ -3,7 +3,7 @@
 ################################################################################
 ## Form generated from reading UI file 'entity_tree_form.ui'
 ##
-## Created by: Qt User Interface Compiler version 5.15.2
+## Created by: Qt User Interface Compiler version 5.15.16
 ##
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
